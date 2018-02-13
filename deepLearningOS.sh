73f9782e1034fe3884fc4d1384f746ee030033c0
Javi <franfdk17@gmail.com>
2018-02-13 14:46:34 +0100

    debian os

---
+++ deepLearningOS.sh
@@ -0,0 +1,32 @@
+# Instalar Gnome
+
+apt-get install gdm3apt-get install  # The display manager
+apt-get install gnome-session        # The session manager
+apt-get install gnome-shell
+apt-get install gnome-keyring libpam-gnome-keyring \
+apt-get install network-manager-gnome \
+apt-get install pulseaudio           # Audio
+apt-get install gnome-terminal       # Aplicación terminal
+apt-get install gnome-control-center # Aplicación ajustes
+apt-get install gnome-tweak-tool     # Aplicación retoques
+
+# Instalar apps
+
+# dpkg-dev -- Debian package development tools
+# libc6-dev -- GNU C Library: Development Libraries and Header Files
+# gcc -- The GNU C compiler
+# g++ -- The GNU C++ compiler, a fairly portable optimizing compiler for C++
+# gfortran -- The GNU Fortran 95 compiler
+# make -- GNU Make
+# check -- A simple interface for defining unit tests
+
+apt-get -q -y install dpkg-dev libc6-dev gcc g++ gfortran make check
+
+# Optimized linear algebra libraries
+apt-get install libopenblas-base libopenblas-dev
+
+# Python
+apt-get -q -y install python python-dev python-pip python3 python3-dev python3-pip
+
+
+apt-get -q -y install git          # git
